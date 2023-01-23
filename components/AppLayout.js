10ac4f38db91bca5921d5a1e19ad0b3f165bdcda
jeongin <kwjddls1919@naver.com>
2023-01-23 17:06:10 +0900

    앱레이아웃 스타일 적용

---
+++ components/AppLayout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Navigation from "./Navigation";
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Navigation />
+    </>
+  );
+};
+
+export default AppLayout;
